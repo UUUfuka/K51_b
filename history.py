4f0a4e2dc184bf4b413eca48ad615a4995dc46b3
UUUfuka <zemelia6@gmail.com>
2025-04-04 11:19:33 +0500

    Kyrsach for analisis

---
+++ history.py
@@ -0,0 +1,95 @@
+import os
+import pandas as pd
+import numpy as np
+from numpy.lib.recfunctions import append_fields
+import struct
+import posidonius.tools
+import posidonius.constants
+from posidonius.particles.axes import Axes
+
+#-------------------------------------------------------------------------------
+# Functions
+#-------------------------------------------------------------------------------
+
+def read(filename):
+    f = open(filename, "rb")
+    # (np.floor(np.log10(np.max((100., 10.)))) - 2.)*10.
+
+    if not os.path.exists(filename):
+        raise Exception("File does not exists!")
+
+    fields = ('current_time', 'time_step', 'particle', 'position_x', 'position_y', 'position_z', 'spin_x', 'spin_y', 'spin_z', 'velocity_x', 'velocity_y', 'velocity_z', 'mass', 'radius', 'radius_of_gyration_2', 'love_number', 'scaled_dissipation_factor', 'lag_angle', 'denergy_dt', 'migration_timescale', )
+
+    data = []
+    while True:
+        try:
+            row = f.read(8+8+4+8*(len(fields)-3))
+            #vrow = struct.unpack('> d d i' + ' d'*(len(fields)-3), row)
+            vrow = struct.unpack('< d d i' + ' d'*(len(fields)-3), row)
+        except:
+            break
+        else:
+            data.append(vrow)
+
+    data = pd.DataFrame(data, columns=fields, index=np.arange(len(data)))
+    if len(data) == 0:
+        raise Exception("Empty file!")
+
+    # Force to always have N lines per snapshot corresponding to N particles
+    n_particles = int(data['particle'].max())+1
+    outer_particles = n_particles-1
+    last_particle = int(data.iloc[-1]['particle'])
+    excess = (n_particles - (outer_particles - last_particle)) % n_particles
+    if excess > 0:
+        data = data[:-1*excess]
+    data = data.to_records()
+    return n_particles, data
+
+def classify(n_particles, data, reference_particle_index=0, discard_first_hundred_years=False):
+    # Ignore first 100 years
+    data['current_time'] /= 365.25 # From days to years
+    if discard_first_hundred_years:
+        data = data[data['current_time'] >= 100.]
+
+    star = data['particle'] == reference_particle_index
+    star_data = data[star]
+
+    planets_data = {}
+    planets_keys = [] # To ensure the order
+    for i in range(n_particles-1):
+        planets_data["{}".format(i+1)] = data[data['particle'] == i+1]
+        planets_keys.append("{}".format(i+1))
+
+    # From inertial to heliocentric coordinates
+    n_data_points = len(star_data['position_x'])
+    zeros = Axes(np.zeros(n_data_points), np.zeros(n_data_points), np.zeros(n_data_points))
+    for key in planets_keys:
+        planets_data[key]['position_x'] -= star_data['position_x']
+        planets_data[key]['position_y'] -= star_data['position_y']
+        planets_data[key]['position_z'] -= star_data['position_z']
+        planets_data[key]['velocity_x'] -= star_data['velocity_x']
+        planets_data[key]['velocity_y'] -= star_data['velocity_y']
+        planets_data[key]['velocity_z'] -= star_data['velocity_z']
+        semimajor_axis = []
+        eccentricity = []
+        inclination = []
+        target_mass = planets_data[key]['mass']
+        target_position = Axes(planets_data[key]['position_x'], planets_data[key]['position_y'], planets_data[key]['position_z'])
+        target_velocity = Axes(planets_data[key]['velocity_x'], planets_data[key]['velocity_y'], planets_data[key]['velocity_z'])
+        masses = [planets_data[k]['mass'] for k in planets_keys if k != key]
+        masses.insert(0, star_data['mass'])
+        positions = [Axes(planets_data[k]['position_x'], planets_data[k]['position_y'], planets_data[k]['position_z']) for k in planets_keys if k != key]
+        positions.insert(0, zeros) # Star is at the center
+        velocities = [Axes(planets_data[k]['velocity_x'], planets_data[k]['velocity_y'], planets_data[k]['velocity_z']) for k in planets_keys if k != key]
+        velocities.insert(0, zeros) # Star is resting
+        a, q, e, i, p, n, l = posidonius.tools.calculate_keplerian_orbital_elements(target_mass, target_position, target_velocity, masses=masses, positions=positions, velocities=velocities)
+        #a, q, e, i, p, n, l = posidonius.tools.calculate_keplerian_orbital_elements(target_mass+star_data['mass'], target_position, target_velocity)
+        planets_data[key] = append_fields(planets_data[key], ('semi-major_axis', 'eccentricity', 'inclination','longitude_of_perihelion','longitude_of_ascending_node','mean_anomaly'), (a, e, i,p,n,l), usemask=False)
+    star_data['position_x'] = 0.
+    star_data['position_y'] = 0.
+    star_data['position_z'] = 0.
+    star_data['velocity_x'] = 0.
+    star_data['velocity_y'] = 0.
+    star_data['velocity_z'] = 0.
+    return star_data, planets_data, planets_keys
+
---
+++ resonances.py
@@ -0,0 +1,190 @@
+"""
+Based on a script developed by Dr. Christophe Cossou
+"""
+import numpy as np
+from fractions import Fraction
+from posidonius.constants import RAD2DEG
+
+#-------------------------------------------------------------------------------
+# Resonances
+#-------------------------------------------------------------------------------
+
+def get_possible_resonances(periodRatio, uncertainty=0.05, denominator_limit=12, numerator_limit=20, sampling=10):
+    """
+    Give a list of 'Fraction' objects that correspond to possible Mean Motion Resonances for a given period ratio.
+    Optional argument are the uncertainty that will determine the range of perio ratios to test around the given value.
+    The denominator and numerator limit above which all the resonances will be skipped
+
+    Parameter :
+    periodRatio : [float] the periodRatio between the two considered parameters
+
+    Optional parameters :
+    uncertainty=0.05 : the period range to test will be [p*(1-0.05) ; p * (1+0.05)]
+    denominator_limit=12 : 13:12 resonance will be ok, but 14:13 resonance will be skipped
+    numerator_limit=20 : 20:12 resonance will be ok, but 21:12 resonance will be skipped
+    sampling=10 : The number of period ratio values for the range of values of period ratio to be tested around the nominal value
+                  (given the uncertainty)
+
+    Return :
+    list of 'Fraction' objects, each one representing a Mean Motion Resonance to be tested
+
+    NOTE: From a package developed by Autiwa <autiwa@gmail.com>
+    """
+
+    periodMin = periodRatio * (1 - uncertainty)
+    periodMax = periodRatio * (1 + uncertainty)
+
+    # We do not want period ratios less than 1 (this only happens for coorbitals I think)
+    if (periodMin < 1.):
+        periodMin = 1.
+
+    periodWidth = periodMax - periodMin
+    deltaPeriod = periodWidth/sampling
+
+
+    periods = [periodMin + deltaPeriod * i for i in range(sampling)]
+
+    resonances = []
+    for period_i in periods:
+        fraction = Fraction(period_i).limit_denominator(denominator_limit)
+        resonances.append(fraction)
+
+    # We exclude all values that appears several time to only keep one occurence of each value
+    resonances = list(set(resonances))
+
+    # We sort the resonances to get the more interesting first (3:2 before 32:27 for instance)
+    tmp = [(res.numerator, res) for res in resonances]
+    tmp.sort()
+    resonances = [element[1] for element in tmp if element[1].numerator < numerator_limit]
+
+    #~ print(uncertainty)
+    #~ print(periodMin)
+    #~ print(periodMax)
+    #~ print(resonances)
+    #~ exit()
+
+    return resonances
+
+def is_resonance(res, g_inner, n_inner, M_inner, g_outer, n_outer, M_outer, nb_points=50, angle_center_value=0, std_threshold=20.):
+    """
+    Given a resonance as a Fraction object, and g, n M for inner and
+    outer planet, the function return if there is the resonance between
+    the two planets
+
+    Parameters :
+    res : a Fraction object (for instance Fraction(3,2))
+    g_inner, n_inner, M_inner : g, n, M for the inner planet (in degrees)
+    g_outer, n_outer, M_outer : g, n, M for the outer planet (in degrees)
+
+    Optional parameters :
+    nb_points : [50] The number of points, at the end the each time span, that we will use to test resonance
+    angler_center_value : [0.] (in degrees) we will overlap all the angle, through congruence, to have angle centered over this value.
+                          For instance, with 0, angle will be between -180 and +180 degrees.
+    std_threshold = [20.] in degrees, the value below which we will consider that a resonant angle prove the existence of a MMR.
+
+    Return :
+    True or False
+
+    NOTE: From a package developed by Autiwa <autiwa@gmail.com>
+    """
+    outer_period_nb = res.denominator
+    inner_period_nb = res.numerator
+
+    angle_min = angle_center_value - 180.
+    angle_max = angle_center_value + 180.
+
+    # Resonances are usually displayed as (p+q):p where q is the order of
+    # the resonance. We retreive thoses parameters
+    p = outer_period_nb
+    q = inner_period_nb - outer_period_nb
+
+    # We calculate the resonant angles
+    long_of_peri_inner = g_inner + n_inner
+    mean_longitude_inner = M_inner + long_of_peri_inner
+
+    long_of_peri_outer = g_outer + n_outer
+    mean_longitude_outer = M_outer + long_of_peri_outer
+
+    phi = np.empty((q+1, int(nb_points))) # we create an array, empty for the moment, that will contain all the resonant angles associated with the supposed resonance.
+
+    temp_value = inner_period_nb * mean_longitude_outer - outer_period_nb * mean_longitude_inner
+
+    for i in range(q+1):
+        phi[i] = temp_value - i * long_of_peri_inner - (q - i) * long_of_peri_outer
+
+    # We take modulo 2*pi of the resonant angle
+    phi = phi%(360.)
+    too_low = phi < angle_min
+    too_high = phi > angle_max
+    phi[too_low] = phi[too_low] + 360.
+    phi[too_high] = phi[too_high] - 360.
+
+    delta_longitude = long_of_peri_outer - long_of_peri_inner
+    delta_longitude = delta_longitude%(360.)
+    too_low = delta_longitude < angle_min
+    too_high = delta_longitude > angle_max
+    delta_longitude[too_low] = delta_longitude[too_low] + 360.
+    delta_longitude[too_high] = delta_longitude[too_high] - 360.
+
+    # If one of the std's is small (Typically, around 25, but
+    # I had once a 80 that was not a resonance, so I think a threshold around 40 is a good one)
+    standard_deviation = min(phi.std(1))
+
+    if (standard_deviation < std_threshold):
+        return True
+    else:
+        return False
+
+def calculate_resonance_data(planets_keys, planets_data):
+    # We initialize the arrays
+    t = [] # time in years
+    a = [] # demi-grand axe en ua
+    e = [] # eccentricity
+    g = [] # argument of pericentre or argument of perihelion (degrees)
+    n = [] # longitude of ascending node (degrees)
+    M = [] # Mean anomaly (degrees)
+    q = [] # periastron or perihelion (AU)
+    Q = [] # apastron or aphelion (AU)
+
+    for key in planets_keys:
+        planet_data = planets_data[key]
+        t.append(planet_data['current_time']) # Years
+        a.append(planet_data['semi-major_axis']) # AU
+        e.append(planet_data['eccentricity'])
+        # Change from longitude of perihelion to argument of perihelion
+        longitude_of_perihelion_degrees = (planet_data['longitude_of_perihelion'] * RAD2DEG) % 360.
+        longitude_of_ascending_node_degrees = (planet_data['longitude_of_ascending_node'] * RAD2DEG) % 360.
+        g.append((longitude_of_perihelion_degrees - longitude_of_ascending_node_degrees) % 360.)
+        n.append(longitude_of_ascending_node_degrees)
+        M.append((planet_data['mean_anomaly'] * RAD2DEG) % 360.)
+        #q.append(planet_data['perihelion_distance'])
+        #Q.append(planet_data['semi-major_axis']*2 - planet_data['perihelion_distance'])
+        q.append(planet_data['semi-major_axis'] * (1 - planet_data['eccentricity']))
+        Q.append(planet_data['semi-major_axis'] * (1 + planet_data['eccentricity']))
+    return t, a, e, g, n, M, q, Q
+
+def get_subplot_shape(number_of_plots):
+    """
+    We chose the number of plot in x and y axis for the p.multi
+    environment in order to plot ALL the resonant angles and have x and
+    y numbers as close on from another as possible.
+
+    HOW TO :
+    fig = pl.figure(1)
+    subplot_i = 0
+    (nb_line, nb_row) = get_subplot_shape(5)
+    subplot_i += 1
+    plot_dof = fig.add_subplot(nb_line, nb_row, subplot_i)
+
+
+    NOTE: From a package developed by Autiwa <autiwa@gmail.com>
+    """
+    nb_plots_x = 1
+    nb_plots_y = 1
+    while (nb_plots_x * nb_plots_y < number_of_plots):
+        if (nb_plots_x == nb_plots_y):
+            nb_plots_y += 1
+        else:
+            nb_plots_x += 1
+
+    return (nb_plots_x, nb_plots_y)
